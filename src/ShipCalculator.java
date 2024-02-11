cb07f06cc6d3361cab369d77982c7da467e8ebfc
emersooc <emersooc@mail.uc.edu>
2024-02-11 17:28:20 -0500

    Finished Task

--- src/ShipCalculator.java
+++ src/ShipCalculator.java
@@ -12,7 +12,7 @@ public class ShipCalculator
         System.out.println("Enter the price of the item: ");
         double userCost = userInput.nextDouble(); //reads user input
 
-        if (userCost >= 100)
+        if (userCost >= 100) //determines if > $100
         {
             shipping = 0.00;
         }
@@ -21,7 +21,7 @@ public class ShipCalculator
             shipping = userCost * 0.02;
         }
 
-        total = userCost + shipping;
+        total = userCost + shipping; //calculates total cost
 
         System.out.printf("The cost of shipping is $%.2f %n",shipping);
         System.out.printf("The total cost is $%.2f",total);
