a7a0455d8dfbf94764aa579aaf2683a5fd049fbe
emersooc <emersooc@mail.uc.edu>
2024-02-11 17:26:49 -0500

    Finished Task

--- src/ShipCalculator.java
+++ src/ShipCalculator.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class ShipCalculator
 {
     public static void main(String[] args)
     {
+        //defining variables
+        double shipping;
+        double total;
 
+        Scanner userInput = new Scanner(System.in); //creates scanner object
+        System.out.println("Enter the price of the item: ");
+        double userCost = userInput.nextDouble(); //reads user input
+
+        if (userCost >= 100)
+        {
+            shipping = 0.00;
+        }
+        else
+        {
+            shipping = userCost * 0.02;
+        }
+
+        total = userCost + shipping;
+
+        System.out.printf("The cost of shipping is $%.2f %n",shipping);
+        System.out.printf("The total cost is $%.2f",total);
     }
 }
